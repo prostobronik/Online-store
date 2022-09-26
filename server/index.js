92f4ad9c33ba9384203963fa01ad2058152ed734
Nikita <nik.nekit.14@mail.ru>
2022-09-26 10:42:30 +0500

    создание модели базы данных

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
-// import dotenv from 'dotenv'
-// dotenv.config()
-// import express from 'express'
-// import sequelize from './db.js'
-
+const models = require('./models/models')
+const cors = require('cors')
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+// app.get('/', (reg, res) => {
+//   res.status(200).json({ message: 'WORKING!!!' })
+// })
 
 const start = async () => {
   try {
