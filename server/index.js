f9cfa1399714e01a87d403cbe968aa29703ff3e8
Nikita <nik.nekit.14@mail.ru>
2022-09-27 10:04:34 +0500

    создание контроллеров

--- server/index.js
+++ server/index.js
@@ -3,15 +3,23 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const router = require('./routes/index')
+const errorHandler = require('./middlware/ErrorhandlingMiddlware')
+const path = require('path')
+
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
 
-// app.get('/', (reg, res) => {
-//   res.status(200).json({ message: 'WORKING!!!' })
-// })
+app.use(fileUpload({}))
+app.use('/api', router)
+
+//Обработка ошибокб последний Мидлвэеер
+app.use(errorHandler)
 
 const start = async () => {
   try {
